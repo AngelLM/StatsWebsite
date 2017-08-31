2e9d95b90076694d198b201e524d92e831474a9c
AngelLM <contacto@angel-lm.com>
2017-08-31 18:50:10 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 PATH=/usr/bin::/usr/local/bin:://usr/sbin
 export DISPLAY=:0
-cd /home/angellm/repos/The-Cocky-Website
-python CockyWeb.py >> logs/errorlog.txt
+cd /home/angellm/repos/StatsWebsite
+python StatsWebsite.py >> logs/errorlog.txt
+python gettingData.py
+date="Automatic commit ""$(date +%Y-%m-%d_%H:%M:%S)"
+git add ./
+git commit -m "$date"
+git push origin master
